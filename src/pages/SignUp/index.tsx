f09c8637a31cdf374a9694279300d3fd5f4dda7a
JoaoVSouto <joaovitorss99@hotmail.com>
2020-11-15 14:51:12 -0300

    Registers sign up input fields on unform

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useCallback } from 'react';
+import { Form } from '@unform/web';
 import { FiMail, FiLock, FiUser, FiArrowLeft } from 'react-icons/fi';
 
 import logoImg from '../../assets/logo.svg';
@@ -8,6 +9,10 @@ import { Button, Input } from '../../components';
 import { Container, Content, Background } from './styles';
 
 const SignUp: React.FC = () => {
+  const handleSubmit = useCallback(data => {
+    console.log(data);
+  }, []);
+
   return (
     <Container>
       <Background />
@@ -15,7 +20,7 @@ const SignUp: React.FC = () => {
       <Content>
         <img src={logoImg} alt="GoBarber" />
 
-        <form>
+        <Form onSubmit={handleSubmit}>
           <h1>Faça seu cadastro</h1>
 
           <Input name="name" icon={FiUser} type="text" placeholder="Nome" />
@@ -28,7 +33,7 @@ const SignUp: React.FC = () => {
           />
 
           <Button type="submit">Cadastrar</Button>
-        </form>
+        </Form>
 
         <a href="#!">
           <FiArrowLeft size={20} />
