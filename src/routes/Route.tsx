acf7305ab1746eac9d977d08556d60fc5faed69e
JoaoVSouto <joaovitorss99@hotmail.com>
2020-11-16 19:52:09 -0300

    Creates logic for guarding private routes

---
+++ src/routes/Route.tsx
@@ -0,0 +1,43 @@
+import React, { ComponentType, useMemo } from 'react';
+import {
+  RouteProps as ReactDOMRouteProps,
+  Route as ReactDOMRoute,
+  Redirect,
+} from 'react-router-dom';
+
+import { useAuth } from '../hooks/auth';
+
+interface RouteProps extends ReactDOMRouteProps {
+  isPrivate?: boolean;
+  component: ComponentType;
+}
+
+const Route: React.FC<RouteProps> = ({
+  isPrivate = false,
+  component: Component,
+  ...rest
+}) => {
+  const { user } = useAuth();
+
+  const isSigned = useMemo(() => !!user, [user]);
+
+  return (
+    <ReactDOMRoute
+      {...rest}
+      render={({ location }) => {
+        return isPrivate === isSigned ? (
+          <Component />
+        ) : (
+          <Redirect
+            to={{
+              pathname: isPrivate ? '/' : '/dashboard',
+              state: { from: location },
+            }}
+          />
+        );
+      }}
+    />
+  );
+};
+
+export default Route;
