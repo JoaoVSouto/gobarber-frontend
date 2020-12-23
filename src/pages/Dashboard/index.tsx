c368eda400ab37ab15a75cc75548b92ef1a5d7a9
JoaoVSouto <joaovitorss99@hotmail.com>
2020-12-23 11:45:19 -0300

    Creates and styles Schedule and NextAppointment components

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FiPower } from 'react-icons/fi';
+import { FiClock, FiPower } from 'react-icons/fi';
 
 import { useAuth } from '../../hooks/auth';
 
@@ -11,6 +11,10 @@ import {
   HeaderContent,
   Profile,
   ProfileImagePlaceholder,
+  Content,
+  Schedule,
+  NextAppointment,
+  Calendar,
 } from './styles';
 
 import logoImg from '../../assets/logo.svg';
@@ -44,6 +48,34 @@ const Dashboard: React.FC = () => {
           </button>
         </HeaderContent>
       </Header>
+
+      <Content>
+        <Schedule>
+          <h1>Horários agendados</h1>
+          <p>
+            <span>Hoje</span>
+            <span>Dia 06</span>
+            <span>Segunda-feira</span>
+          </p>
+
+          <NextAppointment>
+            <strong>Atendimento a seguir</strong>
+            <div>
+              <img
+                src="https://avatars1.githubusercontent.com/u/42191629?v=4"
+                alt="João Vítor"
+              />
+
+              <strong>João Vítor</strong>
+              <span>
+                <FiClock size={20} />
+                08:00
+              </span>
+            </div>
+          </NextAppointment>
+        </Schedule>
+        <Calendar />
+      </Content>
     </Container>
   );
 };
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -67,3 +67,92 @@ export const ProfileImagePlaceholder = styled.span`
   border-radius: 50%;
   background-color: #312e38;
 `;
+
+export const Content = styled.main`
+  max-width: 112rem;
+  margin: 6.4rem auto;
+  display: flex;
+`;
+
+export const Schedule = styled.div`
+  flex: 1;
+  margin-right: 12rem;
+
+  h1 {
+    font-size: 3.6rem;
+  }
+
+  p {
+    margin-top: 0.8rem;
+    color: #ff9000;
+    font-weight: 500;
+
+    span + span {
+      &::before {
+        content: '|';
+        margin: 0 0.8rem;
+      }
+    }
+  }
+`;
+
+export const NextAppointment = styled.div`
+  margin-top: 6.4rem;
+
+  > strong {
+    color: #999591;
+    font-size: 2rem;
+    font-weight: 400;
+  }
+
+  div {
+    background-color: #3e3b47;
+    display: flex;
+    align-items: center;
+    padding: 1.6rem 2.4rem;
+    border-radius: 10px;
+    margin-top: 2.4rem;
+    position: relative;
+
+    &::before {
+      content: '';
+      position: absolute;
+      height: 71%;
+      width: 2px;
+      background-color: #ff9000;
+      border-radius: 0 10px 10px 0;
+      left: 0;
+      top: 50%;
+      transform: translateY(-50%);
+    }
+
+    img {
+      height: 8rem;
+      width: 8rem;
+      border-radius: 50%;
+      object-fit: cover;
+    }
+
+    strong {
+      margin-left: 2.4rem;
+      color: #f4ede8;
+      font-size: 2.4rem;
+    }
+
+    span {
+      margin-left: auto;
+      display: flex;
+      align-items: center;
+      color: #999591;
+
+      svg {
+        color: #ff9000;
+        margin-right: 0.8rem;
+      }
+    }
+  }
+`;
+
+export const Calendar = styled.aside`
+  width: 38rem;
+`;
