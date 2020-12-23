728c00c7effd311a6f3014d247dbc286cd85ef68
JoaoVSouto <joaovitorss99@hotmail.com>
2020-12-23 10:26:26 -0300

    Creates and styles dashboard header

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,36 @@
 import React from 'react';
+import { FiPower } from 'react-icons/fi';
 
-// import { Container } from './styles';
+import { Container, Header, HeaderContent, Profile } from './styles';
+
+import logoImg from '../../assets/logo.svg';
 
 const Dashboard: React.FC = () => {
-  return <h1>hello dashboard!</h1>;
+  return (
+    <Container>
+      <Header>
+        <HeaderContent>
+          <img src={logoImg} alt="GoBarber" />
+
+          <Profile>
+            <img
+              src="https://avatars1.githubusercontent.com/u/42191629?v=4"
+              alt="João Vítor"
+            />
+
+            <div>
+              <span>Bem vindo,</span>
+              <strong>João Vítor</strong>
+            </div>
+          </Profile>
+
+          <button type="button">
+            <FiPower size={20} />
+          </button>
+        </HeaderContent>
+      </Header>
+    </Container>
+  );
 };
 
 export default Dashboard;
---
+++ src/pages/Dashboard/styles.ts
@@ -0,0 +1,57 @@
+import styled from 'styled-components';
+
+export const Container = styled.div``;
+
+export const Header = styled.header`
+  padding: 3.2rem 0;
+  background-color: #28262e;
+`;
+
+export const HeaderContent = styled.div`
+  max-width: 112rem;
+  margin: 0 auto;
+  display: flex;
+  align-items: center;
+
+  > img {
+    height: 8rem;
+  }
+
+  button {
+    margin-left: auto;
+    background-color: transparent;
+    border: 0;
+
+    svg {
+      color: #999591;
+    }
+  }
+`;
+
+export const Profile = styled.div`
+  display: flex;
+  align-items: center;
+  margin-left: 8rem;
+
+  img {
+    width: 5.6rem;
+    height: 5.6rem;
+    border-radius: 50%;
+    object-fit: cover;
+  }
+
+  div {
+    display: flex;
+    flex-direction: column;
+    margin-left: 1.6rem;
+    line-height: 2.4rem;
+
+    span {
+      color: #f4ede8;
+    }
+
+    strong {
+      color: #ff9000;
+    }
+  }
+`;
